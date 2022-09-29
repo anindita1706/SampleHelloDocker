9d35b381259bff3817bcddc4ffed02491ba6e20c
anindita1706 <111552384+anindita1706@users.noreply.github.com>
2022-09-29 22:13:24 +0530

    Add files via upload

---
+++ SampleHelloDocker/src/test/java/SampleHelloDocker/TestHelloDocker.java
@@ -0,0 +1,13 @@
+package SampleHelloDocker;
+
+import org.testng.annotations.Test;
+
+public class TestHelloDocker {
+	
+	@Test
+	public void TestHelloDocker() {
+		
+		System.out.println("Hello, Welcome to Docker");
+	}
+
+}
